6a956eeca47aac70ced3f8cded11f8b796d63e12
Daniyal Syed <daniyal.syed@live.com>
2024-01-10 22:41:27 -0600

    after task 6 all test passing

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -2,47 +2,46 @@ package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
 
-public class CoreCompetency {
+public class CoreCompetency extends JobField{
 
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    public CoreCompetency() {
-        id = nextId;
-        nextId++;
-    }
+//    private int id;
+//    private static int nextId = 1;
+//    private String value;
+//
+//    public CoreCompetency() {
+//        id = nextId;
+//        nextId++;
+//    }
 
     public CoreCompetency(String value) {
-        this();
-        this.value = value;
+        super(value);
     }
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
 
-    public int getId() {
-        return id;
-    }
-
-    public static int getNextId() {
-        return nextId;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public static int getNextId() {
+//        return nextId;
+//    }
+//
+//    public String getValue() {
+//        return value;
+//    }
+//
+//    public void setValue(String value) {
+//        this.value = value;
+//    }
 
 
     // Custom toString, equals, and hashCode methods:
 
     @Override
     public String toString() {
-        return value;
+        return super.toString();
     }
 
     @Override
@@ -50,12 +49,12 @@ public class CoreCompetency {
         if (this == o) return true;
         if (!(o instanceof CoreCompetency)) return false;
         CoreCompetency that = (CoreCompetency) o;
-        return id == that.id;
+        return getId() == that.getId();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return super.hashCode();
     }
 
 
